f73d7bf2bc250222f8607671bfd6a367a56980be
valencsoares <pvcampossoares558@gmail.com>
2024-06-10 17:41:33 -0400

    Criado as providências do handlebars e do express, juntamente com as rotas

--- index.js
+++ index.js
@@ -2,9 +2,28 @@ require("dotenv").config();
 const conn = require("./db/conn");
 
 const Usuario = require("./models/Usuario.js")
+const handlebars = require("express-handlebars")
+const express = require("express")
+const app = express()
+
+app.engine("handlebars", handlebars.engine())
+app.set("view engine", "handlebars")
+
+app.use(express.urlencoded({urlencoded: true,}));
+app.use(express.json())
+
+app.get("/usuarios/novo", (req, res) => {
+    res.render(`formUsuario`)
+})
+app.get("/", (req, res) => {
+    res.render(`home`)
+})
+app.listen(8000, () => {
+    console.log("Aplicação rodando!")
+})
 
 conn.sync().then(() => {
-    console.log("Conectado e sincronizado ao bando de dados! :)");
+    console.log("Conectado e sincronizado ao banco de dados com sucesso! XD");
 }).catch((err) => {
     console.log("Ocorreu um erro: " + err)
 });
